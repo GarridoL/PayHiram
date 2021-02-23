c500920e71fdeaccc094831a335f53f2addc7db5
kennck <kennettecanales@gmail.com>
2021-02-23 16:53:26 +0800

    update

--- App.js
+++ App.js
@@ -8,9 +8,6 @@ import { createAppContainer } from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
 import { Helper } from 'common';
 import SystemVersion from 'services/System.js';
-
-// import { fcmService } from 'services/broadcasting/FCMService';
-// import { localNotificationService } from 'services/broadcasting/LocalNotificationService';
 const AppContainer = createAppContainer(AppNavigation);
 
 class ReduxNavigation extends React.Component{
@@ -20,7 +17,6 @@ class ReduxNavigation extends React.Component{
 
 
   componentDidMount(){
-    this.firebaseNotification()    
     this.getTheme()
     SystemVersion.checkVersion(response => {
       this.setState({isLoading: false})
@@ -28,103 +24,6 @@ class ReduxNavigation extends React.Component{
       }
     })
   }
-
-  // firebaseNotification(){
-  //   const { user } = this.props.state;
-  //   if(user == null){
-  //     return
-  //   }
-  //   fcmService.registerAppWithFCM()
-  //   fcmService.register(this.onRegister, this.onNotification, this.onOpenNotification)
-  //   localNotificationService.configure(this.onOpenNotification, 'Payhiram')
-  //   return () => {
-  //     console.log("[App] unRegister")
-  //     fcmService.unRegister()
-  //     localNotificationService.unRegister()
-  //   }
-  // }
-
-  // onRegister = (token) => {
-  //   console.log("[App] onRegister", token)
-  // }
-
-  // onNotification = (notify) => {
-  //   console.log("[App] onNotification", notify)
-  //   const options = {
-  //     soundName: 'default',
-  //     playSound: true
-  //   }
-
-  //   localNotificationService.showNotification(
-  //     0,
-  //     notify.title,
-  //     notify.body,
-  //     notify,
-  //     options,
-  //     "test"
-  //   )
-  // }
-
-
-  firebaseNotification(){
-    const { user, messengerGroup } = this.props.state;
-    if(user == null){
-      return
-    }
-    fcmService.registerAppWithFCM()
-    fcmService.registerOnOpen(this.onRegister, this.onNotification)
-    fcmService.subscribeTopic('Message-' + user.id)
-    // return () => {
-    //   console.log("[App] unRegister")
-    //   fcmService.unRegister()
-    // }
-  }
-
-  onRegister = (token) => {
-    console.log("[App] onRegister", token)
-  }
-
-  onNotification = (notify) => {
-    const { user } = this.props.state;
-    console.log("[Messages] onNotification", notify)
-    let { data } = notify
-    if(user == null || data == null){
-      return
-    }
-    switch(data.topic){
-      case 'Message': {
-        const { messengerGroup } = this.props.state;
-        if(messengerGroup == null){
-          if(parseInt(data.account_id) != user.id){
-            const { setUnReadMessages } = this.props;
-            setUnReadMessages(data);
-          }
-        }else{
-          if(parseInt(data.messenger_group_id) === messengerGroup.id && parseInt(data.account_id) != user.id){
-            const { updateMessagesOnGroup } = this.props;
-            updateMessagesOnGroup(data);
-          }
-        }
-      }
-      break;
-    }
-
-    // const options = {
-    //   soundName: 'default',
-    //   playSound: true
-    // }
-
-    // localNotificationService.showNotification(
-    //   0,
-    //   notify.title,
-    //   notify.body,
-    //   notify,
-    //   options,
-    //   "test"
-    // )
-  }
-
-
 
   getTheme = async () => {
     try {
