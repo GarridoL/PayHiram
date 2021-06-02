05b30da222bd67ed04417e7ad7a8e14baac0bbb6
= <=>
2021-06-02 23:34:18 +0800

    initial on logout inactivity

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Dimensions, PanResponder, Platform } from 'react-native';
+import { View, Dimensions, PanResponder, Platform, Modal, TouchableOpacity, Text } from 'react-native';
 import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from '@redux';
@@ -12,14 +12,18 @@ const AppContainer = createAppContainer(AppNavigation);
 const height = Math.round(Dimensions.get('window').height);
 const width = Math.round(Dimensions.get('window').width);
 import BackgroundTimer from 'react-native-background-timer';
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import Button from 'components/Form/Button';
+import { Color, BasicStyles } from 'common'
 class ReduxNavigation extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       timer: false,
       timeForInactivityInSecond: 1,
-      interval: null
+      interval: null,
+      showModal: false
     }
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponderCapture: () => {
@@ -37,7 +41,6 @@ class ReduxNavigation extends React.Component{
       if(response == true){
       }
     })
-    this.resetInactivityTimeout()
   }
 
   getTheme = async () => {
@@ -67,11 +70,35 @@ class ReduxNavigation extends React.Component{
     console.log("[App] onOpenNotification", notify )
   }
 
+  incrementTime = () => {
+    console.log(this.state.timer)
+    this.setState({
+      timer: this.state.timer + 1
+    })
+  }
+
+  deAuthenticate = () => {
+    const { logout, setActiveRoute } = this.props;
+    this.setState({
+      showModal: false,
+      timer: 0
+    })
+    logout();
+  }
+
   resetInactivityTimeout = () => {
     const { timer } = this.state;
+    const { user } = this.props.state;
 
-    if(timer > 20){
+    if(user == null){
+      return
+    }
+
+    if(timer > 10){
       console.log('show modal here')
+      this.setState({
+        showModal: true
+      })
     }else{
       BackgroundTimer.stopBackgroundTimer()
       
@@ -87,7 +114,8 @@ class ReduxNavigation extends React.Component{
   }
 
   render(){
-    const { acceptPayment, user } = this.props.state
+    const { acceptPayment, user, theme } = this.props.state
+    const { showModal } = this.state;
     return (
       <View style={{
         flex: 1
@@ -95,6 +123,114 @@ class ReduxNavigation extends React.Component{
       {...this.panResponder.panHandlers}
       >
         <AppContainer />
+        {
+          showModal && (
+            <Modal visible={showModal}>
+              <View style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                flex: 1
+              }}>
+                <View style={{
+                  minHeight: 100,
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                  paddingTop: 20,
+                  paddingBottom: 20,
+                  borderRadius: 12,
+                  width: '80%',
+                  marginRight: '10%',
+                  marginLeft: '10%',
+                  backgroundColor: 'white'
+                }}>
+
+                    {
+                      /*Action buttons*/
+                    }
+
+                    <View style={{
+                      width: '100%',
+                      alignItems: 'center'
+                    }}>
+                      <FontAwesomeIcon icon={faExclamationTriangle} size={50} color={Color.danger}/>
+                    </View>
+
+                    <Text style={{
+                      fontSize: 16,
+                      fontWeight: 'bold',
+                      paddingTop: 10,
+                      textAlign: 'center',
+                    }}>
+                      Security Alert!
+                    </Text>
+
+                    <Text style={{
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      paddingTop: 50,
+                      paddingBottom: 50,
+                      textAlign: 'center',
+                      paddingLeft: 20,
+                      paddingRight: 20,
+                      color: Color.danger
+                    }}>
+                      You've have been away for the passed minutes. We want to make sure it's you!
+                    </Text>
+
+                    <View style={{
+                      flexDirection: 'row',
+                      width: '100%',
+                      justifyContent: 'space-between',
+                      alignItems: 'space-between'
+                    }}>
+                      <Button
+                        onClick={() => {
+                          this.deAuthenticate()
+                        }}
+                        title={'Logout'}
+                        style={{
+                          borderColor: Color.danger,
+                          borderWidth: 1,
+                          width: '45%',
+                          height: 50,
+                          borderRadius: 25,
+                          backgroundColor: 'transparent'
+                        }}
+                        textStyle={{
+                          color: Color.danger,
+                          fontSize: BasicStyles.standardFontSize
+                        }}
+                      />
+
+
+                      <Button
+                        onClick={() => {
+                          this.setState({
+                            showModal: false,
+                            timer: 0
+                          })
+                          this.resetInactivityTimeout()
+                        }}
+                        title={"Continue using"}
+                        style={{
+                          backgroundColor: theme ? theme.secondary : Color.secondary,
+                          width: '45%',
+                          height: 50,
+                          borderRadius: 25,
+                        }}
+                        textStyle={{
+                          color: Color.white,
+                          fontSize: BasicStyles.standardFontSize
+                        }}
+                      />
+                    </View>
+                </View>
+              </View>
+
+            </Modal>
+          )
+        }
       </View>
     )
   }
@@ -106,7 +242,9 @@ const mapDispatchToProps = dispatch => {
   return {
     setTheme: (theme) => dispatch(actions.setTheme(theme)),
     setUnReadMessages: (messages) => dispatch(actions.setUnReadMessages(messages)),
-    updateMessagesOnGroup: (message) => dispatch(actions.updateMessagesOnGroup(message))
+    updateMessagesOnGroup: (message) => dispatch(actions.updateMessagesOnGroup(message)),
+    logout: () => dispatch(actions.logout()),
+    setActiveRoute: (route) => dispatch(actions.setActiveRoute(route))
   };
 };
 let AppReduxNavigation = connect(mapStateToProps, mapDispatchToProps)(ReduxNavigation)
